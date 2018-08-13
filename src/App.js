a29a2a614ae3a330f5893d07353511fac95bcc47
Matt Wood <m.wood0904@gmail.com>
2018-08-13 13:14:59 -0500

    changed lint staged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import logo from './images/cratebind-logo.png';
 
 const Styles = styled.div`
